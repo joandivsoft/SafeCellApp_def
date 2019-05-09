8bc49aa802c5202fc6d0542ec5e052c6a71f8ea8
iaw36984781 <iaw36984781@h05.informatica.escoladeltreball.org>
2019-05-09 17:30:12 +0200

    commit vdef

--- app/src/main/java/com/example/prova01/MainActivity.java
+++ app/src/main/java/com/example/prova01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.prova01;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -36,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         final EditText eName=findViewById(R.id.fieldName);
         final EditText ePassword=findViewById(R.id.fieldPassword);
         FloatingActionButton fab = findViewById(R.id.fab);
+        Intent intent;
+
+        final SharedPreferences pref;
+        pref = getSharedPreferences("user_details",MODE_PRIVATE);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +71,10 @@ public class MainActivity extends AppCompatActivity {
 
                                     JSONObject jsonObject = new JSONObject(response);
                                     Utilities.getToken = jsonObject.getString("access_token");
+                                    SharedPreferences.Editor editor = pref.edit();
+                                    //editor.putString("username",ename);
+                                    //editor.putString("password",password);
+                                    editor.commit();
                                     //Toast.makeText(getApplicationContext(), "Get token: "+Utilities.getToken, Toast.LENGTH_LONG).show();
                                     //==============
 
