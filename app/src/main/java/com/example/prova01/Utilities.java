dc62d64774641fcecbc914fa2bfc6d56e8d0303b
iaw36984781 <iaw36984781@h05.informatica.escoladeltreball.org>
2019-05-13 17:48:42 +0200

    versio v3

--- app/src/main/java/com/example/prova01/Utilities.java
+++ app/src/main/java/com/example/prova01/Utilities.java
@@ -9,17 +9,35 @@ public class Utilities {
             "Benvingut a l'aplicació SafeCellApp",
             "Sessió iniciada",
             "L'aplicació està funcionant",
-            "Compte"};
+            "Compte",
+            "Geolocalització",
+            "Connexions",
+            "Mòbil",
+            "Lectures",
+            ""
+    };
     static final String[] msg_es={
             "Bienvenido a la aplicación SafeCellApp",
             "Sessión iniciada",
             "La aplicación está corriendo",
-            "Advertencia"};
+            "Advertencia",
+            "Geolocalización",
+            "Conexiones",
+            "Móvil",
+            "Lecturas",
+            ""
+    };
     static final String[] msg_en={
             "Wellcome at SafeCellApp",
             "Session initied",
             "App is srunning",
-            "Warning"};
+            "Warning",
+            "Geolocalization",
+            "Connections",
+            "Mobile",
+            "Read data",
+            ""
+    };
 
     static String[][] lang1={msg_ca, msg_es, msg_en};
     static int language1=0;
@@ -27,9 +45,15 @@ public class Utilities {
     //static String numIMEI="355595073424522";
     static String numIMEI="860921033593787";
     static String getToken="";
+    static String getGPS="";
+    static String getInternet="";
+
+    //Girona
+    //41.983333333333
+    // 2.8166666666667
 
     //https://safe-cell.herokuapp.com/dashboard
 
-    
+
 
 }
